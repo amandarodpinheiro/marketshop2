11f15155d6f7cc696b1e1ead5c07ffb94d271e52
amandarodpinheiro <amanda.pinheiro@fatec.sp.gov.br>
2024-04-25 10:54:42 -0300

    atualizaçao email, botao

--- resources/views/cria-conta.blade.php
+++ resources/views/cria-conta.blade.php
@@ -54,8 +54,9 @@
 
       <div>
         <!-- Form -->
-        <form>
-          <div class="lg:max-w-lg lg:mx-auto lg:me-0 ms-auto">
+        <form action="{{route('salva-usuario')}}" method=post>
+        @csrf  
+        <div class="lg:max-w-lg lg:mx-auto lg:me-0 ms-auto">
             <!-- Card -->
             <div class="p-4 sm:p-7 flex flex-col bg-white rounded-2xl shadow-lg dark:bg-neutral-900">
               <div class="text-center">
@@ -87,7 +88,7 @@
                   <div>
                     <!-- Floating Input -->
                     <div class="relative">
-                      <input type="text" id="hs-hero-signup-form-floating-input-first-name" class="peer p-4 block w-full border-gray-200 rounded-lg text-sm placeholder:text-transparent focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:focus:ring-neutral-600
+                      <input type="text" name="nome" id="hs-hero-signup-form-floating-input-first-name" class="peer p-4 block w-full border-gray-200 rounded-lg text-sm placeholder:text-transparent focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:focus:ring-neutral-600
                       focus:pt-6
                       focus:pb-2
                       [&:not(:placeholder-shown)]:pt-6
@@ -108,30 +109,7 @@
                   </div>
                   <!-- End Input Group -->
 
-                  <!-- Input Group -->
-                  <div>
-                    <!-- Floating Input -->
-                    <div class="relative">
-                      <input type="text" id="hs-hero-signup-form-floating-input-last-name" class="peer p-4 block w-full border-gray-200 rounded-lg text-sm placeholder:text-transparent focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:focus:ring-neutral-600
-                      focus:pt-6
-                      focus:pb-2
-                      [&:not(:placeholder-shown)]:pt-6
-                      [&:not(:placeholder-shown)]:pb-2
-                      autofill:pt-6
-                      autofill:pb-2" placeholder="Doe">
-                      <label for="hs-hero-signup-form-floating-input-last-name" class="absolute top-0 start-0 p-4 h-full text-sm truncate pointer-events-none transition ease-in-out duration-100 border border-transparent origin-[0_0] dark:text-white peer-disabled:opacity-50 peer-disabled:pointer-events-none
-                        peer-focus:scale-90
-                        peer-focus:translate-x-0.5
-                        peer-focus:-translate-y-1.5
-                        peer-focus:text-gray-500 dark:peer-focus:text-neutral-500
-                        peer-[:not(:placeholder-shown)]:scale-90
-                        peer-[:not(:placeholder-shown)]:translate-x-0.5
-                        peer-[:not(:placeholder-shown)]:-translate-y-1.5
-                        peer-[:not(:placeholder-shown)]:text-gray-500 dark:peer-[:not(:placeholder-shown)]:text-neutral-500 dark:text-neutral-500">Last name</label>
-                    </div>
-                    <!-- End Floating Input -->
-                  </div>
-                  <!-- End Input Group -->
+          
 
                   <!-- Input Group -->
                   <div>
@@ -158,30 +136,7 @@
                   </div>
                   <!-- End Input Group -->
 
-                  <!-- Input Group -->
-                  <div>
-                    <!-- Floating Input -->
-                    <div class="relative">
-                      <input type="text" id="hs-hero-signup-form-floating-input-company-name" class="peer p-4 block w-full border-gray-200 rounded-lg text-sm placeholder:text-transparent focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:focus:ring-neutral-600
-                      focus:pt-6
-                      focus:pb-2
-                      [&:not(:placeholder-shown)]:pt-6
-                      [&:not(:placeholder-shown)]:pb-2
-                      autofill:pt-6
-                      autofill:pb-2" placeholder="Preline">
-                      <label for="hs-hero-signup-form-floating-input-company-name" class="absolute top-0 start-0 p-4 h-full text-sm truncate pointer-events-none transition ease-in-out duration-100 border border-transparent origin-[0_0] dark:text-white peer-disabled:opacity-50 peer-disabled:pointer-events-none
-                        peer-focus:scale-90
-                        peer-focus:translate-x-0.5
-                        peer-focus:-translate-y-1.5
-                        peer-focus:text-gray-500 dark:peer-focus:text-neutral-500
-                        peer-[:not(:placeholder-shown)]:scale-90
-                        peer-[:not(:placeholder-shown)]:translate-x-0.5
-                        peer-[:not(:placeholder-shown)]:-translate-y-1.5
-                        peer-[:not(:placeholder-shown)]:text-gray-500 dark:peer-[:not(:placeholder-shown)]:text-neutral-500 dark:text-neutral-500">Company name</label>
-                    </div>
-                    <!-- End Floating Input -->
-                  </div>
-                  <!-- End Input Group -->
+          
 
                   <!-- Input Group -->
                   <div class="relative col-span-full">
@@ -270,43 +225,11 @@
                   </div>
                   <!-- End Input Group -->
 
-                  <!-- Input Group -->
-                  <div class="col-span-full">
-                    <!-- Floating Input -->
-                    <div class="relative">
-                      <input type="password" id="hs-hero-signup-form-floating-input-current-password" class="peer p-4 block w-full border-gray-200 rounded-lg text-sm placeholder:text-transparent focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:focus:ring-neutral-600
-                      focus:pt-6
-                      focus:pb-2
-                      [&:not(:placeholder-shown)]:pt-6
-                      [&:not(:placeholder-shown)]:pb-2
-                      autofill:pt-6
-                      autofill:pb-2" placeholder="********">
-                      <label for="hs-hero-signup-form-floating-input-current-password" class="absolute top-0 start-0 p-4 h-full text-sm truncate pointer-events-none transition ease-in-out duration-100 border border-transparent origin-[0_0] dark:text-white peer-disabled:opacity-50 peer-disabled:pointer-events-none
-                        peer-focus:scale-90
-                        peer-focus:translate-x-0.5
-                        peer-focus:-translate-y-1.5
-                        peer-focus:text-gray-500 dark:peer-focus:text-neutral-500
-                        peer-[:not(:placeholder-shown)]:scale-90
-                        peer-[:not(:placeholder-shown)]:translate-x-0.5
-                        peer-[:not(:placeholder-shown)]:-translate-y-1.5
-                        peer-[:not(:placeholder-shown)]:text-gray-500 dark:peer-[:not(:placeholder-shown)]:text-neutral-500 dark:text-neutral-500">Current password</label>
-                    </div>
-                    <!-- End Floating Input -->
-                  </div>
-                  <!-- End Input Group -->
+            
                 </div>
                 <!-- End Grid -->
 
-                <!-- Checkbox -->
-                <div class="mt-5 flex items-center">
-                  <div class="flex">
-                    <input id="remember-me" name="remember-me" type="checkbox" class="shrink-0 mt-0.5 border-gray-200 rounded text-blue-600 focus:ring-blue-500 dark:bg-neutral-900 dark:border-neutral-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800">
-                  </div>
-                  <div class="ms-3">
-                    <label for="remember-me" class="text-sm dark:text-white">I accept the <a class="text-blue-600 decoration-2 hover:underline font-medium dark:text-blue-500" href="#">Terms and Conditions</a></label>
-                  </div>
-                </div>
-                <!-- End Checkbox -->
+                
 
                 <div class="mt-5">
                   <button type="submit" class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">Get started</button>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
@@ -12,3 +13,6 @@ Route::view('/cria-conta', 'home');
 Route::view('/testedeconteudo', 'teste');
 
 Route::view('/cria-conta', '/cria-conta');
+route::post('salva-usuario', function (Request $request) {
+    dd($request);
+})->name('salva-usuario');
